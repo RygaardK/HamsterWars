119440ac526414f959c57cb5b621fae6e2c94c94
Kristoffer Rygaard <rygaardkristoffer@gmail.com>
2022-04-19 13:17:12 +0200

    Till Andy!

---
+++ DataAccessLibrary/Models/HamsterModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLibrary.Models
+{
+    public class HamsterModel
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public int MyProperty { get; set; }
+        public int Age { get; set; }
+        public string FavFood { get; set; }
+        public string Loves { get; set; }
+        public string ImgSrc { get; set; }
+    }
+}
